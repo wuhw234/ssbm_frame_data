322d5a49ac9329b7920fb61ee36138d0ae2b1044
Andy Wu <andywurocks123@gmail.com>
2022-01-01 22:36:53 -0700

    added swagger, changed path to /api

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ from flask_restful import Resource, Api, abort
 from flask_pymongo import MongoClient
 import os
 from dotenv import load_dotenv
+from flask_swagger_ui import get_swaggerui_blueprint
+from werkzeug.utils import send_from_directory
 
 load_dotenv()
 username = os.environ["MONGO_USERNAME"]
 password = os.environ["MONGO_PASSWORD"]
 
-print(username, password)
 cluster = MongoClient(f"mongodb+srv://{username}:{password}@cluster0.acrjr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = cluster["melee_frame_data"]
 collection = db["characters"]
@@ -16,17 +17,41 @@ collection = db["characters"]
 app = Flask(__name__)
 api = Api(app)
 
+@app.route("/static/<path:path>")
+def swagger(path):
+    return send_from_directory("static", path)
+
+
+SWAGGER_URL = "/swagger"
+PATH_TO_SWAGGER = "/static/swagger.json"
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    PATH_TO_SWAGGER,
+    config={
+        "app_name": "Melee Frame Data API"
+    }
+)
+
+
+app.register_blueprint(swaggerui_blueprint)
+
 class Character(Resource):
     def get(self, character_name):
         try:
-            return collection.find_one({"name":character_name}), 200
+            character = collection.find_one({"name":character_name})
+            if not character:
+                raise KeyError()
+            return character, 200
         except:
             abort(404, message="Character does not exist")
 class MoveType(Resource):
     def get(self, character_name, move_type):
         try:
             character = collection.find_one({"name":character_name})
-            return character[move_type], 200
+            movetype_data = character[move_type]
+            if not movetype_data:
+                raise KeyError()
+            return movetype_data, 200
         except:
             abort(404, message="Move Type or Character does not exist")
 class Move(Resource):
@@ -34,15 +59,17 @@ class Move(Resource):
         try:
             character = collection.find_one({"name":character_name})
             moves = character[move_type]
-            return moves[move_name], 200
+            move_data = moves[move_name]
+            if not move_data:
+                raise KeyError()
+            return move_data, 200
         except:
             abort(404,  message="Move, Move Type, or Character does not exist")
 
-        
 
-api.add_resource(Character, "/<string:character_name>")
-api.add_resource(MoveType, "/<string:character_name>/<string:move_type>")
-api.add_resource(Move, "/<string:character_name>/<string:move_type>/<string:move_name>")
+api.add_resource(Character, "/api/<string:character_name>")
+api.add_resource(MoveType, "/api/<string:character_name>/<string:move_type>")
+api.add_resource(Move, "/api/<string:character_name>/<string:move_type>/<string:move_name>")
 
 if __name__ == "__main__":
     app.run()
